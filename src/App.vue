0f2a9b53f333917c574ae6f5efcc09f86a517fcb
muyi-jian <2628168756@qq.com>
2024-05-19 09:27:14 +0800

    登录页面开发

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,8 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <router-view></router-view>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
