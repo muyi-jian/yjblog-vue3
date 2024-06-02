59ca25ee81d7d0fa30330a0480e9d3ca256091df
muyi-jian <2628168756@qq.com>
2024-06-02 15:25:56 +0800

    分类模块实现

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <router-view></router-view>
+  <!-- 设置语言为中文 -->
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 
 </template>
+<script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
+</script>
+
 
 <style scoped>
 /* 自定义顶部加载 Loading 颜色 */
