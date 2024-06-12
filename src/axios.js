a4bf105cde8994a14118838d45960d9bcacf6e13
muyi-jian <2628168756@qq.com>
2024-06-12 23:29:27 +0800

    首页模块实现

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { showMessage} from '@/composables/util'
 import { getToken ,removeToken } from "@/composables/cookie"
+import { useUserStore } from '@/stores/user'
 
 
 // 创建 Axios 实例
@@ -37,8 +38,9 @@ instance.interceptors.response.use(function (response) {
 
     // 状态码 401
     if (status == 401) {
-        // 删除 cookie 中的令牌
-        removeToken()
+        // 退出登录
+        let userStore = useUserStore()
+        userStore.logout()
         // 刷新页面
         location.reload()
     }
--- src/layouts/admin/components/AdminFooter.vue
+++ src/layouts/admin/components/AdminFooter.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="bg-white py-5 flex items-center justify-center text-sm text-gray-500 shadow-none">
     <!-- Copyright 版权信息 -->
-    Copyright © 2023. All rights reserved. Provided by&nbsp; <a class="underline" href="https://www.quanxiaoha.com" target="_blank">犬小哈</a>
+    Copyright © 2023. All rights reserved. Provided by&nbsp; <a class="underline" href="https://www.quanxiaoha.com" target="_blank">木白</a>
   </div>
 </template>
 
